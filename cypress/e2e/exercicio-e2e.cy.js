9c9c99213c06b3836ed35c22e53ae65dccc7af04
pedro gomes <pedro.gonka02@gmail.com>
2024-04-12 13:47:01 -0300

    exercicios ebac

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 import PesquisarPage from "../support/page_objects/Pesquisar.page";
-import { faker } from '@faker-js/faker'; 
+import { faker } from '@faker-js/faker';
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
@@ -15,45 +15,61 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     cy.visit('/minha-conta/');
   });
 
-  beforeEach(() => {
-    cy.visit('/minha-conta/');
-  });
-
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
     cy.fixture('Perfil').then(dados => {
       cy.get('#username').type(dados.usuario);
-      cy.get('#password').type(dados.senha, {log: false});
+      cy.get('#password').type(dados.senha, { log: false });
       cy.get('.woocommerce-form > .button').click();
-      cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá,');
-
-      cy.fixture('produtos').then(produtos => {
-        for (let i = 0; i < produtos.length; i++) {
-          const produto = produtos[i];
-          PesquisarPage.buscarProduto(produto.nomeProduto);
-          PesquisarPage.addProdutoCarrinho(
-            produto.tamanho, 
-            produto.cor, 
-            produto.quantidade
-          );
-          cy.get('.woocommerce-message').should('contain', produto.nomeProduto);
-        }
-      });
-
-      cy.get('.woocommerce-message > .button').click();
-      cy.get('.checkout-button').click();
-
-      cy.preencherDados(
-        faker.person.firstName(), 
-        faker.person.lastName(),
-        faker.location.streetAddress(),
-        faker.location.secondaryAddress(),
-        faker.location.city(),
-        '13221510',
-        '11 954739281',
-      )
-    cy.wait(5000)
-    cy.get('.woocommerce-order-details__title').should('contain', 'Detalhes do pedido')
-
+      cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá,');
     });
+
+    cy.fixture('produtos').then(dados => {
+      PesquisarPage.buscarProduto(dados[0].nomeProduto);
+      PesquisarPage.addProdutoCarrinho(
+        dados[0].tamanho,
+        dados[0].cor,
+        dados[0].quantidade
+      );
+      cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto);
+
+      PesquisarPage.buscarProduto(dados[1].nomeProduto);
+      PesquisarPage.addProdutoCarrinho(
+        dados[1].tamanho,
+        dados[1].cor,
+        dados[1].quantidade
+      );
+      cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto);
+
+      PesquisarPage.buscarProduto(dados[2].nomeProduto);
+      PesquisarPage.addProdutoCarrinho(
+        dados[2].tamanho,
+        dados[2].cor,
+        dados[2].quantidade
+      );
+      cy.get('.woocommerce-message').should('contain', dados[2].nomeProduto);
+
+      PesquisarPage.buscarProduto(dados[3].nomeProduto);
+      PesquisarPage.addProdutoCarrinho(
+        dados[3].tamanho,
+        dados[3].cor,
+        dados[3].quantidade
+      );
+      cy.get('.woocommerce-message').should('contain', dados[3].nomeProduto);
+    });
+
+    cy.get('.woocommerce-message > .button').click();
+    cy.get('.checkout-button').click();
+
+    cy.preencherDados(
+      faker.person.firstName(),
+      faker.person.lastName(),
+      faker.location.streetAddress(),
+      faker.location.secondaryAddress(),
+      faker.location.city(),
+      '13221510',
+      '11 954739281'
+    );
+    cy.wait(5000);
+    cy.get('.woocommerce-order-details__title').should('contain', 'Detalhes do pedido');
   });
 });

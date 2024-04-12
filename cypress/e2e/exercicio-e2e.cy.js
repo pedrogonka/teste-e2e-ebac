9517447c6575f318194e75c4d9d08677ab820d21
pedro gomes <pedro.gonka02@gmail.com>
2024-04-12 13:14:10 -0300

    exercicios teste

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import PesquisarPage from "../support/page_objects/Pesquisar.page";
+import { faker } from '@faker-js/faker'; 
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
@@ -10,13 +12,48 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       E validando minha compra ao final */
 
   beforeEach(() => {
-      cy.visit('/')
+    cy.visit('/minha-conta/');
+  });
+
+  beforeEach(() => {
+    cy.visit('/minha-conta/');
   });
 
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
+    cy.fixture('Perfil').then(dados => {
+      cy.get('#username').type(dados.usuario);
+      cy.get('#password').type(dados.senha, {log: false});
+      cy.get('.woocommerce-form > .button').click();
+      cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá,');
+
+      cy.fixture('produtos').then(produtos => {
+        for (let i = 0; i < produtos.length; i++) {
+          const produto = produtos[i];
+          PesquisarPage.buscarProduto(produto.nomeProduto);
+          PesquisarPage.addProdutoCarrinho(
+            produto.tamanho, 
+            produto.cor, 
+            produto.quantidade
+          );
+          cy.get('.woocommerce-message').should('contain', produto.nomeProduto);
+        }
+      });
+
+      cy.get('.woocommerce-message > .button').click();
+      cy.get('.checkout-button').click();
+
+      cy.preencherDados(
+        faker.person.firstName(), 
+        faker.person.lastName(),
+        faker.location.streetAddress(),
+        faker.location.secondaryAddress(),
+        faker.location.city(),
+        '13221510',
+        '11 954739281',
+      )
+    cy.wait(5000)
+    cy.get('.woocommerce-order-details__title').should('contain', 'Detalhes do pedido')
+
+    });
   });
-
-
-})
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,19 @@
-Cypress.Commands.add('login', (usuario, senha) => {
+Cypress.Commands.add('login', (usuario, senha) => { 
     cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, {log: false})
+    cy.get('#password').type(senha)
     cy.get('.woocommerce-form > .button').click()
-});
+})
 
+Cypress.Commands.add('preencherDados', (nome, sobrenome, rua, numero, cidade, cep, telefone) => {
+
+    cy.get('#billing_first_name').clear().type(nome)
+    cy.get('#billing_last_name').clear().type(sobrenome)
+    cy.get('#billing_address_1').clear().type(rua)
+    cy.get('#billing_address_2').clear().type(numero)
+    cy.get('#billing_city').clear().type(cidade)
+    cy.get('#billing_postcode').clear().type(cep)
+    cy.get('#billing_phone').clear().type(telefone)
+    cy.get('#payment_method_cheque').click()
+    cy.get('#terms').click()
+    cy.get('#place_order').click()
+})
